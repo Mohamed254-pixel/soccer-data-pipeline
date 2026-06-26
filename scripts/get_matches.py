e75735c1746de252be1b31e44d79fb8447b3e523
mohamedibrahim <kadaa254@gmail.com>
2026-06-26 10:51:38 -0700

    Generate first soccer match CSV

--- scripts/get_matches.py
+++ scripts/get_matches.py
@@ -1,1 +1,11 @@
-print("Soccer Data Pipeline Started")
+import csv
+
+with open("data/matches.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+
+    writer.writerow(["home_team", "away_team", "home_goals", "away_goals"])
+
+    writer.writerow(["Argentina", "France", 3, 3])
+    writer.writerow(["Spain", "Brazil", 2, 1])
+
+print("CSV file created")
